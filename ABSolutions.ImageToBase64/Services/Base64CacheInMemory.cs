7b5312c9db074f97354e7bb1122de179a2777d25
Asif Bacchus <asif@asifbacchus.dev>
2024-10-05 15:40:42 -0600

    feat(cache): In-memory implementation

--- ABSolutions.ImageToBase64/Services/Base64CacheInMemory.cs
+++ ABSolutions.ImageToBase64/Services/Base64CacheInMemory.cs
@@ -4,14 +4,34 @@ namespace ABSolutions.ImageToBase64.Services;
 
 public class Base64CacheInMemory : IBase64Cache
 {
+    private readonly List<Base64CachedObject> _base64CachedObjects = [];
+
     public async ValueTask<(bool result, Exception? exception)> RegisterAsync(string filename, string base64,
         int expiryMinutes)
     {
-        throw new NotImplementedException();
+        // delete existing
+        var existingBase64Obj = _base64CachedObjects.FirstOrDefault(i => i.Filename == filename);
+        if (existingBase64Obj is not null) _base64CachedObjects.Remove(existingBase64Obj);
+
+        // register new object
+        try
+        {
+            _base64CachedObjects.Add(new Base64CachedObject
+            {
+                Filename = filename,
+                Base64String = base64,
+                Expiry = expiryMinutes == 0 ? null : DateTime.UtcNow.AddMinutes(expiryMinutes)
+            });
+            return await ValueTask.FromResult<(bool result, Exception? exception)>((true, null));
+        }
+        catch (Exception e)
+        {
+            return await ValueTask.FromResult<(bool, Exception?)>((false, e));
+        }
     }
 
     public async ValueTask<Base64CachedObject?> GetCachedBase64(string filename)
     {
-        throw new NotImplementedException();
+        return await ValueTask.FromResult(_base64CachedObjects.FirstOrDefault(i => i.Filename == filename));
     }
 }
