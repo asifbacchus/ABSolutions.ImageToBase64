acc168dd50193941c5bf4018345dc8128c468397
Asif Bacchus <asif@asifbacchus.dev>
2024-10-22 01:06:56 -0600

    feat(demo): Add logging to api demo
    Add correlation logging and JSON logging output to API demo.

--- ABSolutions.ImageToBase64.Demo.Api/Program.cs
+++ ABSolutions.ImageToBase64.Demo.Api/Program.cs
@@ -1,3 +1,4 @@
+using System.Text.Json;
 using ABSolutions.ImageToBase64.Demo.Api;
 using ABSolutions.ImageToBase64.DependencyInjection;
 using ABSolutions.ImageToBase64.Models;
@@ -6,6 +7,15 @@ using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddLogging(logBuilder => logBuilder.AddJsonConsole(opts =>
+{
+    opts.IncludeScopes = true;
+    opts.TimestampFormat = "[HH:mm:ss]";
+    opts.JsonWriterOptions = new JsonWriterOptions
+    {
+        Indented = true
+    };
+}));
 builder.Services.AddHttpClient(
     builder.Configuration.GetRequiredSection(Base64ConverterConfiguration.AppSettingsKey)
         .Get<Base64ConverterConfiguration>()?.HttpClientName ?? "Base64ConverterClient", client =>
@@ -23,7 +33,9 @@ app.MapGet("/base64", async (IBase64Converter converter, bool cache = false) =>
 {
     try
     {
-        var imageAsBase64String = await converter.GetImageAsBase64Async("500.webp", cache);
+        var loggingCorrelationValue = Guid.NewGuid().ToString();
+        var imageAsBase64String =
+            await converter.GetImageAsBase64Async("500.webp", cache, loggingCorrelationValue: loggingCorrelationValue);
         return Results.Ok(imageAsBase64String);
     }
     catch (Exception e)
@@ -40,9 +52,11 @@ app.MapGet("/picture", async (IBase64Converter converter, bool cache = false) =>
 {
     try
     {
+        var loggingCorrelationValue = Guid.NewGuid().ToString();
         const string htmlTemplate =
             "<!DOCTYPE html><html><head><title>Base64 Image Test</title></head><body><img src=\"{0}\" alt=\"image embedded as base64 string\" /></body></html>";
-        var imageAsBase64String = await converter.GetImageAsBase64Async("500.webp", cache);
+        var imageAsBase64String =
+            await converter.GetImageAsBase64Async("500.webp", cache, loggingCorrelationValue: loggingCorrelationValue);
         var htmlResults = string.Format(htmlTemplate, imageAsBase64String);
         return new CustomHtmlResult(htmlResults);
     }
